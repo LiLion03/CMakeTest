475c789c386f8ee481c216a9dcaf0285ebb0df81
Ignatchenko Lev <headhunter16@bk.ru>
2022-08-16 23:57:09 +0300

    My first cmake build

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int number;
+	cout << "enter your number, please: \n";
+	cin >> number;
+	cout << "Your number is ", number;
+	return 0;
+}
